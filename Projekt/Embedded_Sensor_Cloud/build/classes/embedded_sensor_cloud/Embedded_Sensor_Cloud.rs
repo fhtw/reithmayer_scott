58437875481e5629202318a270b30179281f533a
scotty21 <samantha.scott@gmx.at>
2013-10-16 19:48:22 +0200

    SWE code

---
+++ Projekt/Embedded_Sensor_Cloud/build/classes/embedded_sensor_cloud/Embedded_Sensor_Cloud.rs
@@ -0,0 +1,4 @@
+embedded_sensor_cloud.WebServer
+embedded_sensor_cloud.HttpConstants
+embedded_sensor_cloud.Worker
+embedded_sensor_cloud.SimpleWebServer
---
+++ SWE1_embedded_sensor_cloud/src/swe1_embedded_sensor_cloud/client_test.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package swe1_embedded_sensor_cloud;
+
+
+import java.io.*;
+import java.net.*;
+import java.security.*;
+
+/**
+ *
+ * @author if12b061
+ */
+public class client_test {
+    public static void main(String[] args) throws IOException {
+//    Socket c = new Socket("127.0.0.1",4444);
+
+    Socket echoSocket = null;
+    PrintWriter out = null;
+    BufferedReader in = null;
+
+    try {
+            echoSocket = new Socket("127.0.0.1",4444);
+            out = new PrintWriter(echoSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(
+                                        echoSocket.getInputStream()));
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host: taranis.");
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for "
+                               + "the connection to: taranis.");
+            System.exit(1);
+        }
+
+    BufferedReader stdIn = new BufferedReader(
+                                   new InputStreamReader(System.in));
+	String userInput;
+
+	while ((userInput = stdIn.readLine()) != null) {
+	    out.println(userInput);
+	    System.out.println("echo: " + in.readLine());
+	}
+
+	out.close();
+	in.close();
+	stdIn.close();
+	echoSocket.close();
+
+}
+}
